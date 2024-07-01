c456da616bf71cb053457087aa449febda8ba600
sicalo330 <noah.esteban9@gmail.com>
2024-07-01 10:26:32 -0500

    Eliminar librería que daban error segun unity

--- Player.cs
+++ Player.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
-using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class Player : MonoBehaviour
